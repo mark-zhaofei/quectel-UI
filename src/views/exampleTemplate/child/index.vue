50ffa50f1ff62eb32e27e909142a60ca0657e4dc
emma.wang <emma.wang@quectel.com>
2020-08-13 20:02:19 +0800

    add

---
+++ src/views/exampleTemplate/child/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="padding10">
+    <el-button type="primary" @click="goPage('/401')">跳转 401</el-button>
+    <el-button type="primary" @click="goPage('/403')">跳转 403</el-button>
+    <el-button type="primary" @click="goPage('/404')">跳转 404</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goPage(path) {
+      this.$router.push(path)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
